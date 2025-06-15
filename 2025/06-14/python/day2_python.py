5300a924ae2ddeb4021c0eec9d8c66ee2b63ffbf
Cyb3rHunter1337 <Mongwoiching2080@gmail.com>
2025-06-16 00:56:52 +0600

    Create day2_python.py

---
+++ 2025/06-14/python/day2_python.py
@@ -0,0 +1,10 @@
+age = 25
+price = 19.99
+grade = 'A'
+is_student = True
+
+print("Python")
+print(f"Age: {age}")
+print(f"Price: {price}")
+print(f"Grade: {grade}")
+print(f"Is Student: {is_student}")
