15d68740dd58867615f6a5a99cfca8eeef569325
Cyb3rHunter1337 <Mongwoiching2080@gmail.com>
2025-06-16 00:20:43 +0600

    Create day2_cpp.cpp

---
+++ 2025/06-14/cpp/day2_cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    double price = 19.99;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "C++\n";
+    cout << "Age: " << age << "\n";
+    cout << "Price: " << price << "\n";
+    cout << "Grade: " << grade << "\n";
+    cout << "Is Student: " << boolalpha << isStudent << "\n";
+
+    return 0;
+}
