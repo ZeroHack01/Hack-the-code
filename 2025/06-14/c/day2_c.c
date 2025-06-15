efa1d28da5182ac4d1438b1b9f28e2c3488ac08f
Cyb3rHunter1337 <Mongwoiching2080@gmail.com>
2025-06-16 00:31:54 +0600

    Create day2_c.c

---
+++ 2025/06-14/c/day2_c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    double price = 19.99;
+    char grade = 'A';
+    _Bool isStudent = 1; // 1 for true, 0 for false
+
+    printf("C\n");
+    printf("Age: %d\n", age);
+    printf("Price: %.2f\n", price);
+    printf("Grade: %c\n", grade);
+    printf("Is Student: %d\n", isStudent);
+
+    return 0;
+}
